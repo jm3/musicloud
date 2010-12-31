fe322cba8b6b63838a98da8e53bc3f8f2ed185b3
John Manoogian III <jm3@jm3.net>
2010-12-31 15:33:46 -0800

    initial commit

---
+++ build-mp3-grouping-tag-cloud.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+require "mp3info"
+require "find"
+
+songs_dir = '~/Dropbox/Music/'
+# you should need to change nothing below this line:
+
+tags = {}
+songs_dir = File.expand_path( songs_dir )
+Find.find( songs_dir ) do |song|
+  next unless song =~ /mp3$/
+  begin
+    Mp3Info.open( song ) do |id3|
+      song_tags = id3.tag2.TIT1.to_s.split( "," ).collect{ |x| x.strip }
+      song_tags.each do |tag| 
+        tags.has_key?( tag ) ? tags[tag] += 1 : tags[tag] = 1
+        print "."
+      end
+      print " "
+      STDOUT.flush
+    end
+  rescue
+    print "(sad song)"
+  end
+end
+puts
+
+# output each tag once per instance; a good format for creating wordle.net tag clouds
+tags.keys.each{ |tag| puts ("#{tag.to_s} " * tags[tag])}
